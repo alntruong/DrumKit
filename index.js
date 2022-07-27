374529370d4d3c5a810b445d862778eeb68be676
Alan <alntruong@ucdavis.edu>
2022-07-27 10:19:55 -0700

    added audio.play( )

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
         var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
     //     alert("I got clicked!");
     });
     
