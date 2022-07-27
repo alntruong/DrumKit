f71ae15970193dae214f5622c5eb71cf37b661a5
Alan <alntruong@ucdavis.edu>
2022-07-27 01:26:09 -0700

    addEventListener to specific buttons using forLoop

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-document.querySelector("button").addEventListener("click", handleClick);
+/*document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick() {
     alert("I got clicked!");
+}*/
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrumButtons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got clicked!");
+    });
+
 }
