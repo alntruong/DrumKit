f0b09dace0829aa7e0f094a73416401a79895ac7
Alan <alntruong@ucdavis.edu>
2022-07-27 21:19:40 -0700

    moved out audio and alert

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-    //     alert("I got clicked!");
+        console.log(this);
+        
     });
  
 }
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//         audio.play();
+    //     alert("I got clicked!");
