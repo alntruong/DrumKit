ad0e05191849a67c2916b802f11736404c3056da
Alan Truong <alntruong@ucdavis.edu>
2022-08-05 16:07:31 -0700

    combined function for both button and keypress

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function handleClick() {
     alert("I got clicked!");
 }*/
 
+// Detecting Button Press
+
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i<numberOfDrumButtons; i++) {
@@ -14,51 +16,59 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
         // //this refers to the object, each dot notation allows us to delve further into the specificity of the object
         var buttonInnerHTML = this.innerHTML;
 
-            switch (buttonInnerHTML) {
-                case "w":
-                    var tom1 = new Audio("sounds/tom-1.mp3");
-                    tom1.play();
-                    break;
+        makeSound(buttonInnerHTML);
 
-                case "a":
-                    var tom2 = new Audio("sounds/tom-2.mp3");
-                    tom2.play();
-                    break;
-
-                case "s":
-                    var tom3 = new Audio("sounds/tom-3.mp3");
-                    tom3.play();
-                    break;
-
-                case "d":
-                    var tom4 = new Audio("sounds/tom-4.mp3");
-                    tom4.play();
-                    break;
-
-                case "j":
-                    var crash = new Audio("sounds/crash.mp3");
-                    crash.play();
-                    break;
-
-                case "k":
-                    var snare = new Audio("sounds/snare.mp3");
-                    snare.play();
-                    break;
-
-                case "l":
-                    var kick = new Audio("sounds/kick-bass.mp3");
-                    kick.play();
-                    break;
-
-                default: console.log(buttonInnerHTML);
-            }
     });
  
 }
 
+//Detecting Keyboard Press
+
 document.addEventListener("keypress", function(event) {
-    console.log(event);
+    makeSound(event.key);
 })
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "k":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        default: console.log(buttonInnerHTML);
+    }
+}
 
 // var audio = new Audio("sounds/tom-1.mp3");
 //         audio.play();
